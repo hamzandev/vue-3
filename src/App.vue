95471d5528ad41fb0c988ad06d656aead5565416
hamzandev <hamzankechil@gmail.com>
2023-12-01 08:43:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <Navbar />
+  <div class="container py-5 mx-auto">
+    <button
+      @click="test"
+      class="px-6 py-2 rounded-lg shadow bg-blue-500 text-white hover:bg-blue-600 duration-200 focus:outline focus:outline-offset-2 focus:outline-blue-400"
+    >
+      Click
+    </button>
+
+    <div class="my-10 grid gap-4 w-1/2">
+      <span
+        class="uppercase shadow bg-neutral-100 px-4 py-2 rounded-lg hover:shadow-lg duration-200 cursor-pointer"
+        v-for="person in persons"
+        @click="show(person.name)"
+        :key="person.name"
+        >{{ person.name }}</span
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+  data: () => {
+    return {
+      persons: [
+        { name: "Hamzan", age: 19 },
+        { name: "Fikro Najiah", age: 20 },
+        { name: "Muhammad Helmi", age: 20 },
+        { name: "Imam Ikhlasul", age: 23 },
+      ],
+    };
+  },
+  methods: {
+    test: () => {
+      console.log("Hello Coyy!");
+      alert("Halloooo");
+    },
+    show: (name: string) => {
+      alert(name);
+    },
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="bg-blue-500 py-4 shadow">
+    <div class="container mx-auto flex items-center justify-between">
+      <span class="text-lg font-bold text-white uppercase">Logo</span>
+      <div class="flex text-neutral-300 text-lg items-center gap-x-8">
+        <a
+          v-for="menu in links"
+          :key="menu.label"
+          class="hover:text-neutral-100 duration-200"
+          :href="menu.url"
+          >{{ menu.label }}</a
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => {
+    return {
+      links: [
+        { label: "Home", url: "/" },
+        { label: "Price", url: "/price" },
+        { label: "About", url: "/about" },
+        { label: "Contact", url: "/contact" },
+      ],
+    };
+  },
+};
+</script>
